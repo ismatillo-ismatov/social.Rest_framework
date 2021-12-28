db91a3d81bace7ad8afad4de8af6ff25e4fad668
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2021-12-28 09:55:35 +0500

    yangi

--- chat/views.py
+++ chat/views.py
@@ -11,40 +11,23 @@ from user_profile.permissions import IsOwnerReadOnly
 from .models import Message
 from .serializers import *
 
-# @csrf_exempt
-# def user_list(request,pk=None):
-#     if request.method == "GET":
-#         if pk:
-#             users = User.objects.filter(id=pk)
-#         else:
-#             users = User.objects.all()
-#         serializer = UserSerializer(users,many=True,context={"request":request})
-#         return JsonResponse(serializer.data,safe=False)
-#     elif request.method == "POST":
-#         data = JSONParser().parse(request)
-#         serializer = UserSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data,status=201)
-#         return JsonResponse(serializer.errors,status=400)
-
-
-# @csrf_exempt
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerReadOnly]
-    def post(request, receiver=None):
+    def post(request,sender=None ,receiver=None):
         if request.method == "GET":
-            messages = Message.objects.filter(receiver_id=receiver)
+            messages = Message.objects.filter(sender_id=sender,receiver_id=receiver)
             serializer = MessageSerializer(messages,many=True, context={"request":request})
             return JsonResponse(serializer.data,safe=False)
         elif request.method == "POST":
-            data =  JSONParser().parse(request)
+            data = JSONParser().parse(request)
             serializer = MessageSerializer(data=data)
             if serializer.is_valid():
                 serializer.save()
                 return JsonResponse(serializer.data,status=201)
             return JsonResponse(serializer.errors,status=400)
-    # def perform_create(self, serializer):
-    #     serializer.save(owner=self.request.user)
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
