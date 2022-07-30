fcbfaaa250e4c2adc59b5d7ab75339227a250cef
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2022-07-31 00:25:21 +0500

    Update message

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
@@ -13,3 +14,9 @@ class Message(models.Model):
         return self.message
     class Meta:
         ordering = ("timestamp",)
+
+def post_user_massage_signal(sender,instance,created,**kwargs):
+    if created:
+        Message.objects.create(user=instance)
+
+post_save.connect(post_user_massage_signal,sender=User)
