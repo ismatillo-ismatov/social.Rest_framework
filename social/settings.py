206d35e7d25a9c188e67dfd0d0b6ff97461cbbfc
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2022-07-30 10:17:23 +0500

    new

--- social/settings.py
+++ social/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = 'django-insecure-sgfe&8!srs^r=b807nsy__4(f@j(*em%36txj#ti-2zt-=-=0!'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mmk1g@kecr%g5dha2zcdbfncbs528!st&#pu%r+#f(u%0y^t7n'
+# SECRET_KEY = 'django-insecure-mmk1g@kecr%g5dha2zcdbfncbs528!st&#pu%r+#f(u%0y^t7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -107,26 +114,12 @@ DATABASES = {
     }
 }
 
-#
-# DATABASES = {
-#
-#     'default': {
-#
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#
-#         'NAME': "postgres",
-#
-#         'USER': 'drmuser',
-#
-#         'PASSWORD': 'crm1234',
-#
-#         'HOST': 'localhost',
-#
-#         'PORT': '5433',
-#
-#     }
-#
-# }
+
+
+
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
