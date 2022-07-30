1a438b7961b92536407fcc1e93e89e47d7d6a18a
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2022-07-31 00:25:21 +0500

    Update message

--- chat/views.py
+++ chat/views.py
@@ -10,25 +10,22 @@ from rest_framework.decorators import action
 from rest_framework import viewsets
 from user_profile.permissions import IsOwnerReadOnly
 from .models import Message
+from rest_framework.permissions import IsAuthenticated
 from .serializers import *
 
 
+
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # @action(detail=True,methods=['post'])
-    def message_list(request,sender, receiver):
-        if request.method == "GET":
-            messages = Message.objects.get(sender_id=sender,receiver_id=receiver)
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerReadOnly]
+    permission_classes = [IsAuthenticated]
     def post(request,sender=None ,receiver=None):
         if request.method == "GET":
-            messages = Message.objects.filter(sender_id=sender,receiver_id=receiver)
+            messages = Message.objects.filter(sender_id=sender,receiver_id=receiver,is_read=False)
             serializer = MessageSerializer(messages,many=True, context={"request":request})
+            for message in messages:
+                message.is_read = True
+                message.save()
             return JsonResponse(serializer.data,safe=False)
         elif request.method == "POST":
             data = JSONParser().parse(request)
@@ -38,6 +35,13 @@ class MessageViewSet(viewsets.ModelViewSet):
                 return JsonResponse(serializer.data,status=201)
             return JsonResponse(serializer.errors,status=400)
 
+    def message_view(self,request,sender,receiver):
+        if request.method == "GET":
+            return render(request,{'users':User.objects.exclude(username=request.user.username),
+                                   'receive':User.objects.get(id=receiver),
+                                   'messages': Message.objects.filter(sender_id=sender,receiver_id=receiver)|
+                                   Message.objects.filter(sender_id=receiver,receiver_id=sender)
+                                   })
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+
+
