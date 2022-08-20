90b6f067f3de709b7c3bb26445514e214ea41ac5
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2022-08-20 15:30:17 +0500

    add anv

--- social/urls.py
+++ social/urls.py
@@ -18,6 +18,22 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
+from rest_framework import permissions
+from rest_framework.authtoken import views
+from users.views import CustomAuthToken
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Media API",
+      default_version='v1',
+      description="Social.media",
+      contact=openapi.Contact(email="ismatilloismatov1995@gmail.com"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +41,13 @@ urlpatterns = [
     path("api-auth/",include('rest_framework.urls')),
     path('api/dj-rest-auth/register/',include('dj_rest_auth.registration.urls')),
     path('',include("chat.urls")),
+    path('swagger<format>.json|.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api-token-auth/',CustomAuthToken.as_view())
 
 ]
+
+
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
