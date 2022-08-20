8726fe14612feb5de253ed4747b4e315f948881c
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2022-08-20 15:30:17 +0500

    add anv

--- chat/views.py
+++ chat/views.py
@@ -45,3 +45,5 @@ class MessageViewSet(viewsets.ModelViewSet):
 
 
 
+
+
--- users/views.py
+++ users/views.py
@@ -1,18 +1,45 @@
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BaseAuthentication
 from rest_framework import viewsets,status
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializer import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 from django.views.decorators.csrf import csrf_exempt
+from drf_yasg.utils import swagger_auto_schema
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+class CustomAuthToken(ObtainAuthToken):
+    # authentication_classes = [SessionAuthentication,BaseAuthentication]
+    # permission_classes = [IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+
 
 
 class UserViewSet(viewsets.ViewSet):
-
+    authentication_classes = [SessionAuthentication,BaseAuthentication]
+    permission_classes = [IsAuthenticated]
+    @swagger_auto_schema(operation_summary='hello')
     def list(self,request):
         queryset = User.objects.all()
         serializer = UserSerializer(queryset,many=True)
         return Response(serializer.data)
+    # @swagger_auto_schema(operation_summary="Create a user account")
     def create(self,request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
