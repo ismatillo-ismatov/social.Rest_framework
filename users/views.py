840ad8a2847536d5720400d33e1c3282a89d2367
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2023-01-09 16:22:41 +0500

    'user'

--- users/views.py
+++ users/views.py
@@ -14,8 +14,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
 class CustomAuthToken(ObtainAuthToken):
-    # authentication_classes = [SessionAuthentication,BaseAuthentication]
-    # permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication,BaseAuthentication]
+    permission_classes = [IsAuthenticated]
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
