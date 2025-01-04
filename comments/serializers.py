dc6327ab83eb4a2f9c3694592ff9376fd8c000b0
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2025-01-04 19:05:57 +0500

    add comment

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,9 +1,52 @@
 from rest_framework import serializers
 from .models import Comment
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields  = ('username',)
 
 class CommentSerializer(serializers.ModelSerializer):
-    commented_by = serializers.ReadOnlyField(source="owner.username")
+    owner = UserSerializer(read_only=True)
+    ownerProfileImage = serializers.SerializerMethodField()
+    ownerUserName = serializers.SerializerMethodField()
+    replies = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
+    like_count = serializers.ReadOnlyField()
+
     class Meta:
         model = Comment
-        fields = ['id', 'comment','comment_image','comment_date','commented_by','post']
+        fields = ['id','owner','post','parent', 'comment',
+                  'comment_image','comment_date','ownerUserName',
+                  'ownerProfileImage','replies','like_count', 'is_liked']
+        extra_kwargs = {
+        'post':{'required':False}
+        }
+
+    def get_ownerUserName(self,obj):
+        if hasattr(obj.owner,'profile') and obj.owner.profile.userName:
+            return str(obj.owner.profile.userName)
+        return None
+
+    def get_ownerProfileImage(self,obj):
+        if hasattr(obj.owner,'profile') and obj.owner.profile.profileImage:
+            return obj.owner.profile.profileImage.url
+        return  None
+
+
+
+    def get_replies(self,obj):
+        replies = obj.replies.all()
+        print(f"Reply for comment{obj.id}:{replies}")
+        return CommentSerializer(replies, many=True).data
+
+
+    def get_is_liked(self,obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return request.user in obj.likes.all()
+        return False
+
+    def get_like_count(self,obj):
+        return obj.likes.count()
--- votes/models.py
+++ votes/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from posts.models import Post
 
 
-class Vote(models.Model):
+class Like(models.Model):
     post = models.ForeignKey(Post,related_name="votes",on_delete=models.CASCADE)
-    up_vote_by = models.ForeignKey("auth.User",related_name='up_vote_user',on_delete=models.CASCADE,default=None,blank=True,null=True)
-    down_vote_by = models.ForeignKey("auth.User",related_name='down_vote_user',on_delete=models.CASCADE,default=None,blank=True,null=True)
+    user = models.ForeignKey("auth.User",related_name='likes',on_delete=models.CASCADE,default=None,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post','user')
 
     def __str__(self):
         return self.post.content
