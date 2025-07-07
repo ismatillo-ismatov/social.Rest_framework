5ff7a3a079f4241b4b40f8d396a1c7f46c96502f
ismatillo-ismatov <ismatilloismatov1995@gmail.com>
2025-07-07 06:20:42 +0500

    ok

--- comments/admin.py
+++ comments/admin.py
@@ -5,4 +5,4 @@ from .models import Comment
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("id",'owner','post','parent','comment','created_at',)
     search_fields = ('comment','owner__username','post__title')
-    list_filter = ('created_at','post')
+    list_filter = ('created_at','post',)
